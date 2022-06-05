574cd41f9cbb72fc67a4b33f786300e185ff291a
Alexandr <caiiiia_podoplelov@mail.ru>
2022-06-05 12:58:30 +0300

    createReducer(@redux/toolkit)

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,45 +1,38 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+  heroesFetching,
+  heroesFetched,
+  heroesFetchingError,
+  heroDeleted,
+  heroCreated
+} from '../actions';
+
+
 const initialState = {
   heroes: [],
   heroesLoadingStatus: 'idle'
 };
 
-
-const heroes = (state = initialState, action) => {
-  switch (action.type) {
-    //////Fetch heroes//////
-    case 'HEROES_FETCHING':
-      return {
-        ...state,
-        heroesLoadingStatus: 'loading'
-      }
-    case 'HEROES_FETCHED':
-      return {
-        ...state,
-        heroes: action.payload,
-        heroesLoadingStatus: 'idle'
-      }
-    case 'HEROES_FETCHING_ERROR':
-      return {
-        ...state,
-        heroesLoadingStatus: 'error'
-      }
-
-    //////Add and remove heroes//////
-    case 'HERO_DELETED':
-      const newHeroes = state.heroes.filter((hero) => hero.id !== action.payload);
-      return {
-        ...state,
-        heroes: newHeroes
-      }
-    case 'HERO_CREATED':
-      const createdHeroes = [ action.payload, ...state.heroes ];
-      return {
-        ...state,
-        heroes: createdHeroes
-      }
-    default: return state
-  };
-};
+const heroes = createReducer(initialState, (builder) => {
+  builder
+    .addCase(heroesFetching, (state) => {
+      state.heroesLoadingStatus = 'loading';
+    })
+    .addCase(heroesFetched, (state, action) => {
+      state.heroes = action.payload;
+      state.heroesLoadingStatus = 'idle';
+    })
+    .addCase(heroesFetchingError, (state) => {
+      state.heroesLoadingStatus = 'error';
+    })
+    .addCase(heroDeleted, (state, action) => {
+      state.heroes = state.heroes.filter((hero) => hero.id !== action.payload);
+    })
+    .addCase(heroCreated, (state, action) => {
+      state.heroes.push(action.payload);
+    })
+    .addDefaultCase(() => {});
+});
 
 
 export default heroes;
